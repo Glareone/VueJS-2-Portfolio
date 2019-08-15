6ffec74c3128cad691d390541f1c73b0ca4cbf83
Glareone <enlarius@gmail.com>
2019-08-15 19:12:30 +0300

    exercise 6 is finished

---
+++ exercises/exercise-6 (Lecture block 7)/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <page-server-status />
+    </div>
+</template>
+
+<script>
+    import ServerStatusPage from './pages/ServerStatusPage.vue';
+
+    export default {
+        components: {
+            'page-server-status': ServerStatusPage,
+        }
+    }
+</script>
+
+<style>
+ .container {
+     display: grid;
+     grid-template-columns: 1fr 80vw 1fr;
+     grid-row-gap: 1.5rem;
+     grid-column-gap: 2rem;
+ }
+</style>
