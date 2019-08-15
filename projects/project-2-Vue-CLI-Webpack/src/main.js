2f3fdebd44cd7104fb1428f17f62420f0b5dd1b6
Glareone <enlarius@gmail.com>
2019-08-15 18:36:33 +0300

    lecture block 7 is finished

--- projects/project-2-Vue-CLI-Webpack/src/main.js
+++ projects/project-2-Vue-CLI-Webpack/src/main.js
@@ -1,5 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Home from './Home';
+
+// Global registration for Home component
+Vue.component('app-servers', Home);
 
 new Vue({
   el: '#app',
