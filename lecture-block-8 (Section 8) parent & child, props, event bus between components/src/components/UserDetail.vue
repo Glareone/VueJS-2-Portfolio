0d81794ec3636c302a728db0e23ff1fc71427a4e
Glareone <enlarius@gmail.com>
2019-08-17 19:35:27 +0300

    passing complex and primitive types with/without type validation. + default value

--- lecture-block-8 (Section 8) parent & child, props, event bus between components/src/components/UserDetail.vue
+++ lecture-block-8 (Section 8) parent & child, props, event bus between components/src/components/UserDetail.vue
@@ -10,9 +10,27 @@
 
 <script>
   export default {
-    // we defined properties like array with names. This elements from array should be defined from outside.
+    // This is the default way how  we could define properties. Like array with names.
+    // This elements from array should be defined from outside.
     // We defined name in User component (parent component).
-    props: ['myName', 'surName'],
+    //props: ['myName', 'surName'],
+
+    // To get props with validation we should use another way (object instead of array initialization):
+    props: {
+      // It could be multiple types for example. String OR Array. If we set up only string - it will use it like string in every situation.
+      //myName: [String, Array]
+
+
+      myName: String,
+      // Another option:
+      // myName: {
+      //   type: String,
+      //   required: true, // optional parameter
+      //   default: 'Name11' // optional parameter
+      //   default: function() { return { name: 'Max' } } // another way how to write default parameter for Object or Array.
+      // }
+
+    },
     methods: {
       // this method also will be called after changing myName. And switchName() result will be up to date.
       switchName() {
