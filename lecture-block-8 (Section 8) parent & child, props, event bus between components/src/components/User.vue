6046f8a0bd245344cbabefcd2f76b53544a1e9f6
Glareone <enlarius@gmail.com>
2019-08-17 19:06:56 +0300

    passing props "name" from the parent to the child

--- lecture-block-8 (Section 8) parent & child, props, event bus between components/src/components/User.vue
+++ lecture-block-8 (Section 8) parent & child, props, event bus between components/src/components/User.vue
@@ -2,13 +2,19 @@
   <div class="component">
     <h1>The User Component</h1>
     <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my name!</button>
     <hr>
     <div class="row">
       <div class="col-xs-12 col-sm-6">
-        <app-user-detail></app-user-detail>
+        <!--
+          Here we bind name property from User component and pass it to user-detail component.
+          Pay attention. if we will write name="name" - it will not work, we will set up string "name", not name from model.
+          That's why we need to use v-bind: or :
+         -->
+        <app-user-detail :name="name" />
       </div>
       <div class="col-xs-12 col-sm-6">
-        <app-user-edit></app-user-edit>
+        <app-user-edit />
       </div>
     </div>
   </div>
@@ -19,9 +25,19 @@
   import UserEdit from './UserEdit.vue';
 
   export default {
+    data: function() {
+      return {
+        name: 'Alex'
+      }
+    },
     components: {
       appUserDetail: UserDetail,
       appUserEdit: UserEdit
+    },
+    methods: {
+      changeName() {
+        this.name = 'Max'
+      }
     }
   }
 </script>
--- lecture-block-8 (Section 8) parent & child, props, event bus between components/src/components/UserDetail.vue
+++ lecture-block-8 (Section 8) parent & child, props, event bus between components/src/components/UserDetail.vue
@@ -2,10 +2,16 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
+    <p>User Name: {{ name }}</p>
   </div>
 </template>
 
 <script>
+  export default {
+    // we defined properties like array with names. This elements from array should be defined from outside.
+    // We defined name in User component (parent component).
+    props: ['name']
+  }
 </script>
 
 <style scoped>
