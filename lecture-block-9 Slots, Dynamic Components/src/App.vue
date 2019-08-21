beff5e33dc4e9a74dc31a8b07d5262c84e748d31
Glareone <enlarius@gmail.com>
2019-08-21 13:42:10 +0300

    slots

---
+++ lecture-block-9 Slots, Dynamic Components/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <!--
+            We will display this in child component using slots.
+            It could be styled directly from child component (where it is used) or from here (parent component).
+            No matter How we would like to draw string info, from variable or directly in HTML
+           -->
+          <h2 slot="quoteTitleBlock">{{ quoteTitle }}</h2>
+          <p slot="paragraph">A wonderful quote</p>
+
+          <!-- slot without name. -->
+          <p>Default Paragraph</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+  export default {
+    data: function() {
+      return {
+        quoteTitle: 'The Quote',
+      }
+    },
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style>
+  p {
+    color: blue;
+  }
+</style>
---
+++ lecture-block-9 Slots, Dynamic Components/src/components/Quote.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <!--
+     To use html code from parent we have to use slots.
+
+     Pay attention on "H2" styling. We could style slot inside child component (here) because slot compiled here.
+     After last changes in VueJS2: It could be styled directly from child component (where it is used) or from parent component.
+
+     name here - is from slot in parent.
+     If slot don't have name - it will reference to DEFAULT slot. Last one is without slot.
+    -->
+
+
+    <slot name="quoteTitleBlock"/>
+    <slot name="paragraph"/>
+    <slot/>
+
+    <!-- Pay attention on this one. We don't have subTitle in parent. And we want the next behavior:
+      If we don't have such subTitle from parent - we would like to provide Default Text or something else.
+    -->
+    <slot name="subTitle">
+      <p>Default Slot information if such slot doesn't exist in parent</p>
+    </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Quote',
+  }
+</script>
+
+<style scoped>
+  div {
+    border: solid 1px #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+
+  h2 {
+    color: red;
+  }
+</style>
