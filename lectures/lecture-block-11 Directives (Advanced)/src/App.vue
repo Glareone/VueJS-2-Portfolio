ebeb870c50acdd86809196bcbdfe5b5f54fba56d
Glareone <enlarius@gmail.com>
2019-08-24 23:43:09 +0300

    custom directives

--- lectures/lecture-block-11 Directives (Advanced)/src/App.vue
+++ lectures/lecture-block-11 Directives (Advanced)/src/App.vue
@@ -11,8 +11,12 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
-        <p v-text="'Some text'">Color Text</p> <!-- some text will be displayed -->
-        <p v-html="'<strong>Strong Text</strong>'"></p><!-- HTML. Need to be sanitized -->
+        <!-- CUSTOM DIRECTIVE USAGE. Colored background color  -->
+        <p v-highlight="'red'">Color Text</p>
+        <!-- :background here is argument. Access to it using binding.arg  -->
+        <p v-highlight2:background="'green'">Color Text</p>
+        <p v-highlight2:anotherArg="'green'">Color Text will be green</p>
+        <p v-highlight2>Color Text will be black because binding.value is undefined (binding.arg is also undefined)</p>
       </div>
     </div>
   </div>
--- lectures/lecture-block-11 Directives (Advanced)/src/main.js
+++ lectures/lecture-block-11 Directives (Advanced)/src/main.js
@@ -1,9 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// CUSTOM DIRECTIVE
+// Will be bound in app.vue
 Vue.directive('highlight', {
+    bind(el, binding, vnode) { // gives us access to the bind element
+      // el.style.backgroundColor = 'green'; //style here is a normal JS property to which we have an access
+      el.style.backgroundColor = binding.value; // binding value will be used to set up the color  v-highlight="'green'"
+    }
+});
 
-}); // custom directive which could be used by v-highlight
+// custom directive which could be used by v-highlight2
+Vue.directive('highlight2', {
+  bind(el, binding, vnode) { // gives us access to the bind element
+    if(binding.arg === 'background') {
+      el.style.backgroundColor = binding.value;
+    } else {
+      el.style.color = binding.value; // otherwise set up the text color
+    }
+  }
+});
 
 new Vue({
   el: '#app',
