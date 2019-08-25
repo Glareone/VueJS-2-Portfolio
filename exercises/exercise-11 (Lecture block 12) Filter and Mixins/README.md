3a5d6bb1a6318a2103ea8766da550c0b98f72fa4
Glareone <enlarius@gmail.com>
2019-08-25 21:52:17 +0300

    exercise is finished

---
+++ exercises/exercise-11 (Lecture block 12) Filter and Mixins/README.md
@@ -0,0 +1,12 @@
+### Exercise 11, Filters and Mixins
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ exercises/exercise-11 (Lecture block 12) Filter and Mixins/src/ComponentWithMixin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <hr>
+    <input type="text" v-model="componentTestText">
+    <p>Original text: {{ componentTestText }}</p>
+    <p>Reversed text: {{ componentTestText | reverseText }}</p>
+    <p>Computed text(reversed): {{ reversedText }}</p>
+    <p>Computed text(counter): {{ counter }}</p>
+  </div>
+</template>
+
+<script>
+  import { mixin } from './mixins';
+
+  export default {
+    name: 'ComponentWithMixin',
+    data() {
+      return {
+        componentTestText: 'component text which will be used in mixin',
+      }
+    },
+    mixins: [mixin]
+  }
+</script>
+
+<style scoped>
+
+</style>
