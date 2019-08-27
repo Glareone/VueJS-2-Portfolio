7df05198e3ec19047f0eb18c75edeb0050fc5e0a
Glareone <enlarius@gmail.com>
2019-08-28 02:01:34 +0300

    routing basics, router-link, active class, header component, router-configuration

--- lectures/lecture-block-15 Routing/src/main.js
+++ lectures/lecture-block-15 Routing/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import { routes } from './routes.js';
+
+// all routes in routes.js
+Vue.use(VueRouter);
+
+// mode history means the url without hash tag.
+// 'hash' or without anything -> http://localhost:8080/#/user
+// 'history' -> http://localhost:8080/user
+// more info: https://router.vuejs.org/ru/guide/essentials/history-mode.html
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 });
