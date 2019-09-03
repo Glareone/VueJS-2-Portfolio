329fb24f660a0da49627ce1d9d88ccaffadd1d88
Glareone <enlarius@gmail.com>
2019-09-04 02:58:50 +0300

    header, Stocks and Stock components with little logic inside Stock

--- projects/project-5-stock-trader/components/stocks/Stocks.vue
+++ projects/project-5-stock-trader/components/stocks/Stocks.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>Stocks Component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
 </template>
 
 <script>
+  import Stock from './Stock.vue';
+
   export default {
-    name: 'Stocks'
+    name: 'Stocks',
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 110 },
+          { id: 2, name: 'Apple', price: 250 },
+          { id: 3, name: 'Google', price: 200 },
+          { id: 4, name: 'Twitter', price: 70 },
+        ]
+      }
+    },
+    components: {
+      appStock: Stock,
+    }
   }
 </script>
 
