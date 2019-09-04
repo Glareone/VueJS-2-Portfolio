27b10824624a1d0af7506570535a39ca013dba0f
Glareone <enlarius@gmail.com>
2019-09-04 03:27:37 +0300

    vuex configuration+methods, adding data.

--- projects/project-5-stock-trader/components/stocks/Stocks.vue
+++ projects/project-5-stock-trader/components/stocks/Stocks.vue
@@ -9,14 +9,9 @@
 
   export default {
     name: 'Stocks',
-    data() {
-      return {
-        stocks: [
-          { id: 1, name: 'BMW', price: 110 },
-          { id: 2, name: 'Apple', price: 250 },
-          { id: 3, name: 'Google', price: 200 },
-          { id: 4, name: 'Twitter', price: 70 },
-        ]
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks;
       }
     },
     components: {
---
+++ projects/project-5-stock-trader/store/modules/stocks.js
@@ -0,0 +1,41 @@
+import StocksData from '../../dummyData/stocks';
+
+const state = {
+  stocks: [],
+};
+
+const getters = {
+  stocks(state) {
+    return state.stocks;
+  }
+};
+
+const mutations = {
+  // something between pure naming and namespaces from constants.
+  'SET_STOCKS'(state, payload) {
+    state.stocks = payload;
+  },
+  'RANDOM_STOCKS'(state) {
+
+  }
+};
+
+const actions = {
+  buyStock({ commit }, order) {
+    commit();
+  },
+  initStocks({ commit }) {
+    commit('SET_STOCKS', StocksData);
+  },
+  randomizeStocks: ({ commit }) => {
+    commit('RANDOM_STOCK');
+  }
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+};
+
